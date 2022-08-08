8cbfe089d802408a8bae63d65f15aeefe50bbce2
Kris M <rkrismellinger@gmail.com>
2022-08-08 15:06:46 -0700

    classwork update 4

---
+++ Week-02-Conditions_and_Loops/Week 2 Conditionals ext.js
@@ -0,0 +1,83 @@
+
+// if ( num1 >= num2 ) {
+//    if ( num3 >= num4 ) {
+//        console.log("Both statements are true.");
+//    } else {
+//       console.log("The first statement is true, but the second is not.");
+//    }
+//}
+//console.log("Program ended.");
+
+//switch(monthNumber){
+//    case 1:
+//        monthName = "January";
+//        break;
+//    case 2:
+//        monthName = "February";
+//        break;
+//    case 3:
+//        monthName = "March";
+//        break;
+//    case 4:
+//        monthName = "April";
+//        break;
+//    case 5:
+//        monthName = "May";
+//        break;
+//    case 6:
+//        monthName = "June";
+//        break;
+//    case 7:
+//        monthName = "July";
+//        break;
+//    case 8:
+//        monthName = "August";
+//        break;
+//    case 9:
+//        monthName = "September";
+//        break;
+//    case 10:
+//        monthName = "October";
+//        break;
+//    case 11:
+//        monthName = "November";
+//        break;
+//    case 12:
+//        monthName = "December";
+//        break;
+//    default:
+//        monthName = "Invalid month";
+//        break;
+//} console.log(monthName);
+
+
+//if (num1 == num2){
+//   console.log ("same");
+//} else if (num1 < num2){
+//    console.log(num1);
+//} else {
+ //   console.log(num2);
+//}
+
+//if (age >= 16){
+//    canDrive=true;
+ //   console.log (True);
+//} else {
+//    console.log(false);
+//}
+
+//if (numberGrade >= 90){
+ //   letterGrade = "A"
+//} else if (numberGrade >= 80){
+//    letterGrade = "B"
+//} else if (numberGrade >=70){
+//    letterGrade = "C"
+//} else if (numberGrade >=60){
+//   letterGrade = "D"
+//} else (letterGrade= "F");{
+//} console.log (letterGrade);
+
+
+//if (money >= costOfSoda){
+//    console.log("I can buy a soda")
+//  }  else {}
