9be271fb81bec3e5cca6d6a5f0bdba4c007f9e90
Kris M <rkrismellinger@gmail.com>
2022-08-15 10:08:24 -0700

    update 2

--- Week-03-Arrays_and_Functions/week-3-lab.js
+++ Week-03-Arrays_and_Functions/week-3-lab.js
@@ -1,34 +1,57 @@
-//let array1 = [1, 5, 6, 9, 10, 14];
+let array1 = [1, 5, 6, 9, 10, 14];
 
 // print the 3rd element in array1
+console.log(array1[2]);
 
 // print the last element in array1
+console.log(array1[5]);
+//console.log(array1[array1.length-2]);
 
 // add 16 and 3 to array1
-
+//console.log(array1[2]+ 16);
+//array1.push(16, 3);
 // sort the array, then print the 3rd element again
-// did it change?
-
+array1.sort();
+console.log(array1[2])
+// did it change? No
 
 // create a variable called myTodoList that holds an empty array
+let myTodoList = [];
 
 // add three todo items to the array using a built-in array method
+myTodoList.push('Learn Coding')
+myTodoList.push('Finish Lego set')
+myTodoList.push('Finish miniatures')
+console.log(myTodoList)
 
 // remove the second item in the array
+console.log(myTodoList.indexOf('Finish Lego set'));
+//console.log(myTodoList.splice(1))
 
 // create another array, yourTodoList, and add two todo items
+let yourTodoList = ['Paint one art', 'Finish table'];
+console.log(yourTodoList)
 
 // create another array, ourTodoList
+let ourTodoList = [];
+
 // combine myTodoList and yourTodoList into ourTodoList
-
-
+ourTodoList = myTodoList.concat(yourTodoList);
+ourTodoList.sort();
+console.log(ourTodoList)
 // sort the following array from Z-A
+ourTodoList.reverse();
+console.log(ourTodoList)
 
 // create a function called reverse that takes in parameter
 // this function will return the opposite of whatever is passed in
 // if its a boolean, return the opposite
 // if its a number, or a string, return the reverse (i.e. 1234 becomes 4321, Name becomes emaN)
 // if its an array, return the reversed array with each element reversed
+let i 
+function reverse (i){
+    
+}
 
 
 // create a function called addingMachine that will add all passed numbers and return the total
