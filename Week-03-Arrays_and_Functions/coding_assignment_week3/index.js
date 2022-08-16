61c52cca02a27eea28b846dc8f9141ede6282722
Kris M <rkrismellinger@gmail.com>
2022-08-16 09:21:28 -0700

    update1 assignment

---
+++ Week-03-Arrays_and_Functions/coding_assignment_week3/index.js
@@ -0,0 +1,13 @@
+let itemPrice = 23.5;
+let moneyInWallet = 125.0;
+let numberOfFriends = 200;
+let ageInYears = 42;
+let firstName = "Ronald";
+let lastName = "Mellinger";
+let middleName = "Kristopher";
+
+//Full name code with added age
+let fullName =
+  "My full name is " + firstName + " " + middleName + " " + lastName + ".";
+console.log(fullName);
+console.log("I am " + ageInYears + "years old.");
