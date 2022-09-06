cdae355d631a5c62359b0211dd0d94c89293f9bf
RKMellinger <rkrismellinger@gmail.com>
2022-09-06 11:33:43 -0700

    update 1

--- Week-06-DevTools_Debugging_and_Unit_Tests/week 6.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/week 6.js
@@ -2,10 +2,50 @@ console.log("JS Final")
 //
 //
 //
-class Deck {
-    constructor(cards){
-
-    }
+let player1
+let player2
+const suits = ["H","D","C","S"]
+const values = ["A","2","3","4","5","6","7","8","9","T","J","Q","K"]
+let fullDeck =[]
+for (let i = 0; i < suits.length; i++){
+    console.log(suits[i])
+}
+for (let i = 0; i < values.length; i++){
+    console.log(values[i])
 }
 
-class Cards
+fullDeck = suits.flatMap
+/*
+
+function buildDeck(){
+    for (i = 0; i < 52; i++){
+        let shuffler = deck[i]
+        let randomizeShuffler = Math.floor(Math.random() * 52)
+    
+    }
+} 
+
+class Deck {
+    constructor(cards){
+        this.cards = cards
+        
+    }
+
+}
+
+class Cards{
+    constructor(suit, value){
+        this.suit = suit
+        this.value = value
+    }
+
+}
+
+class Game{
+    constructor(){
+    let deck = []
+    }
+
+}
+
+*/
