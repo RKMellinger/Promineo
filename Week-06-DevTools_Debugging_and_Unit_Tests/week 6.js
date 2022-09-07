490e3b2375d23c1e1b7c86e05ea853edc688643d
RKMellinger <rkrismellinger@gmail.com>
2022-09-07 12:20:30 -0700

    update 3

--- Week-06-DevTools_Debugging_and_Unit_Tests/week 6.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/week 6.js
@@ -5,6 +5,7 @@ console.log("JS Final")
 // if/else statement for win loss, if index value is greater it's a win.
 let player1 =[]
 let player2 =[]
+let deckSize = 52
 const suits = ["♥","♦","♣","♠"] //Suits array
 const values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"] // Card Value
 //
@@ -14,7 +15,14 @@ function newDeck(){ // Creating deck of cards
             return new Cards(suit, value)
         })
     }) 
-    
+}
+function shuffle(){ //randomly swap card for card
+    for (let i = 0; i < deckSize; i++ ){
+        let firstPosition = Math.floor(Math.random() * (i + 1))
+        let oldPosition = this.cards[firstPosition]
+        this.cards[firstPosition] = this.cards[i]
+        this.cards[i] = oldPosition
+    }        
 }
 
 //
@@ -28,19 +36,14 @@ class Cards { //Framework for the cards
 class Deck { //Code to create the full deck
     constructor(cards = newDeck()){
         this.cards = cards    
-        this.deck = []   
+        this.deck += newDeck.push   
     }
     
 
-    }
-    function shuffle(){ //randomly swap card for card
-        for (let i = 0; i < deck.length + 1; i++ ){
-            let firstPosition = Math.floor(Math.random() * (i + 1))
-            let oldPosition = this.cards[firstPosition]
-            this.cards[firstPosition] = this.cards[i]
-            this.cards[i] = oldPosition
-        }        
-    }
+}
+// let halfDeck = Math.ceil(deckSize / 2)
+// player1 = new Deck(this.deck.split(0,halfDeck))
+// player2 = new Deck(this.deck.split(halfDeck, deckSize))
 
 
 // class CardGame {     //Starting menu for selections
@@ -51,9 +54,7 @@ class Deck { //Code to create the full deck
   
 // function start() {  //Starts the the game
     
-//     const halfDeck =Math.ceil(deckSize / 2)
-//     player1 = new Deck(Deck.cards.splice(0,halfDeck))
-//     player2 = new Deck(Deck.cards.splice(halfDeck, deckSize))
+
 
 //     console.log(player1)
 //     console.log(player2)
@@ -83,14 +84,40 @@ class Deck { //Code to create the full deck
 //           `);
 //     }
 
-    
-   
-//     deal(){ //Will start game
+//    let player1Points =0
+//    let player2Points =0
+//    let tie = 0
+//    let winner = 0
+//     deal(){              //Will start game
 //       let deck = new Deck()
 //       suits.shuffler
 //     }
 //     judge(){
-//         If 
+//      for (i = 0; i < 26; i++)
+//         If (player1[i] > player2[i]){
+//          player1Points++
+//         } else if (player1[i] < player2[i]){
+//          player2Points++ 
+//         }else {
+//          tie++
+//         } 
+//     winner(){
+//        If (player1Points > player2Points){
+//           winner = player1
+//        } else if (player1Points < player2Points){
+//           winner = player2
+//        } else {
+//           winner = "We have a problem"
+//          }
+//         }
+//     console.log(`
+//     Player 1 total points: ${player1Points}
+//     Player 2 total points: ${player2Points}
+//     Total number of ties: ${tie}
+    
+//     The winner is ${winner}
+//     `)
+
 //     }
 
 //   }
@@ -100,7 +127,7 @@ class Deck { //Code to create the full deck
 //   let menu = new Menu() //Starts the prompts rolling
 //   menu.start()
 let fullDeck = new Deck()
-shuffle(fullDeck)
+//shuffle(fullDeck)
 console.log(fullDeck.cards)
 // start(fullDeck.cards)
 
