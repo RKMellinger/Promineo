afde61549ec7f4fb62c8450c91413410160e36ef
RKMellinger <rkrismellinger@gmail.com>
2022-09-09 17:59:08 -0700

    update 5.2

--- Week-06-DevTools_Debugging_and_Unit_Tests/week 6.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/week 6.js
@@ -1,4 +1,4 @@
-class Player {
+class Player { // Player descriptions
   constructor(name) {
       this.name = name
       this.score = 0;
@@ -6,19 +6,20 @@ class Player {
   }
 }
 let names = ["Stockfish 13","Fat Fritz 2","Komodo Dragon","Igel 3.0.5","RubiChess 2.1","Houdini 6","Deep Blue"]
-let player1 = new Player(names[Math.floor(Math.random()*names.length)])
-let player2 = new Player(names[Math.floor(Math.random()*names.length)])
+let player1 = new Player(names[Math.floor(Math.random()*names.length)]) // Lets play Chess, er.. War...
+let player2 = new Player(names[Math.floor(Math.random()*names.length)]) // Assigns a random name to the player
 
-console.log("line 31", player1.name, player1.score, player1.hand)
+console.log("line 31", player1.name, player1.score, player1.hand) 
 console.log("line 32", player2.name, player2.score, player2.hand)
 
-let faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-console.log(faces.length);
+let faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']; //Card components
 let suits = ['hearts', 'diamonds', 'spades', 'clubs'];
 let value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-console.log("line 19",value.length);
+let fullDeck = 52
+let playerDecks = 26
 
-class Card {
+
+class Card { // Card builder
   constructor(faces, suit, value) {
       this.faces = faces;
       this.suit = suit;
@@ -26,48 +27,68 @@ class Card {
   }
 }
 
-class Deck {
+class Deck { //the Deck builder 
   constructor() {
       this.deck = []; 
       this.tie = 0
       this.winner = ""
+      let player1Points = 0
+      let player2Points = 0
   }
 
-  createDeck() {
+  createDeck() { //creates the deck
       for (let facesIndex = 0; facesIndex < faces.length; facesIndex++) {
           for (let suitsIndex = 0; suitsIndex < suits.length; suitsIndex++) {
               this.deck.push(new Card(faces[facesIndex], suits[suitsIndex], value[facesIndex]));
           }
       }
   }
-  shuffle() {
-          for (let i = 52 -1; i > 0; i--) {
+  shuffle() { //ShuffleMaster 
+          for (let i = 52 -1; i > 0; i--) { 
             let j = Math.floor(Math.random() * i)
-            let k = this.card[i]
-            this.card[i] = this.card[j]
-            this.card[j] = k
+            let k = this.cards[i]
+            this.cards[i] = this.cards[j]
+            this.cards[j] = k
     }
   }
-  dealDeck() {
-      player1.hand.push(this.deck.slice(0, 26)); //for loop
-      player2.hand.push(this.deck.slice(26, 52))
-      console.log("line 52",player1.hand)
-      console.log("line 53",player2.hand)
+  dealDeck() { // Deals the on 52 card deck into two 26 player decks
+      player1.hand = (this.deck.slice(0, 26));
+      player2.hand = (this.deck.slice(26, 52))
+      // for(let i=0; i<26; i++){
+      //   if(i % 2 === 0){
+      //   player1.hand.push(this.deck);
+      //   }else{
+      //   player2.hand.push(this.deck)
+      //   }
 
-  }
-  scoreCard(){
-      for (let i = 0; i < 26; i++){
-        if (player1.cards>player2.cards) {
-          player1.score + 1        
-        }else if (player1.cards<player2.cards){
-          player2.score + 1
+      //console.log("line 56",player1.hand[0]);
+
+      //}
+      console.log("line 54",player1.hand)
+      console.log("line 55",player2.hand)
+    }
+  scoreCard(){ // Compares the decks from player 1 and player 2 to determine the winner.
+      // for(let card1 of player1.hand){
+      //   console.log(card1.length)
+      //   console.log("line 60 player 1",card1.value)
+      //   for(let card2 of player2.hand){
+      //     console.log("line 62 player 2",card2.value)
+      //   }
+      // }
+      for (let i = 0; i < playerDecks; i++){
+        //console.log("line 68",player1.hand[0]);
+        if (player1.hand[i]>player2.hand[i]) {
+          console.log('line 70',player1.hand[0],player2.hand[0])
+          this.player1Points + 1        
+        }else if (player1.hand[0]<player2.hand[0]){
+          this.player2Points + 1
         }else {
           this.tie + 1
         }
         console.log(player1.score,player2.score,this.tie)
      }
   }
-  // winning(){
+  // winning(){ //The script for displaying the game and winner.
   //     if (player1.score>player2.score && player1.score>this.tie) {
   //     this.winner = player1
   //       }else if (player1.score< player2.score && player2.score>this.tie){
@@ -85,9 +106,10 @@ class Deck {
   //    `)
   // }
 }
-
+// console.log("line 88", player1.deck)
 
 let freshDeck = new Deck();
 freshDeck.createDeck();
+//freshDeck.shuffle()
 freshDeck.dealDeck();
 freshDeck.scoreCard()
