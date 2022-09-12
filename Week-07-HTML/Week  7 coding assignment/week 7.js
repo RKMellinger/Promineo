1a0c2ada7770c8e2831b41f1111f041e52ed4f7f
RKMellinger <rkrismellinger@gmail.com>
2022-09-12 15:23:03 -0700

    Week 7 Update 1.0

---
+++ Week-07-HTML/Week  7 coding assignment/week 7.js
@@ -0,0 +1,19 @@
+console.log(`Greetings Program, 
+This is the week 7 coding assignment. 
+As a but of fyi, I am part of a 501c3 
+historical reenactment group and by 
+the end of this I would like to be 
+able to build a website for them. 
+If you don't mind I'll start working 
+on it here. :D
+Thank you for your time.`)
+
+alert(`Greetings Program, 
+This is the week 7 coding assignment. 
+As a bit of fyi, I am part of a 501c3 
+historical reenactment group and by 
+the end of this I would like to be 
+able to build a website for them. 
+If you don't mind I'll start working 
+on it here. :D
+Thank you for your time.`)
