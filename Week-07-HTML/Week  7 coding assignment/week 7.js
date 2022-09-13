6105efebb8205b7ee01aca4f97457f83f15abdf5
RKMellinger <rkrismellinger@gmail.com>
2022-09-13 08:46:22 -0700

    update 2.1

--- Week-07-HTML/Week  7 coding assignment/week 7.js
+++ Week-07-HTML/Week  7 coding assignment/week 7.js
@@ -17,3 +17,39 @@ able to build a website for them.
 If you don't mind I'll start working 
 on it here. :D
 Thank you for your time.`)
+
+//Magic Cannonball
+
+let userQuestion = `Please enter your question..`
+console.log(userQuestion)
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber)
+let cannonBall = ""
+
+switch (randomNumber){
+case randomNumber = 0 :
+cannonBall = 'Yar, It be certain'
+break
+case randomNumber = 1:
+ cannonBall = 'I feel it in my bones'
+break
+ case (randomNumber = 2):
+ cannonBall = 'The map says treasure abound'
+break
+ case (randomNumber = 3):
+  cannonBall = 'Ya, might walk the plank..'
+break
+ case (randomNumber = 4):
+ cannonBall = 'Dead Men Tell No Tales'
+break
+ case (randomNumber = 5):
+ cannonBall = 'set the sails and rigging!'
+break
+ case (randomNumber = 6):
+ cannonBall = 'Batten Down the Hatches'
+default:
+ cannonBall = 'The mists say come back later'
+ break
+}
+
+console.log(cannonBall)
