1cb0f7c664cc6f8c18fbeeece1e3ed68d10ab871
RKMellinger <rkrismellinger@gmail.com>
2022-09-13 21:26:49 -0700

    Updete 2.4

--- Week-07-HTML/Week  7 coding assignment/week 7.js
+++ Week-07-HTML/Week  7 coding assignment/week 7.js
@@ -1,24 +1,18 @@
 console.log(`Greetings Program, 
-This is the week 7 coding assignment. 
-As a but of fyi, I am part of a 501c3 
-historical reenactment group and by 
-the end of this I would like to be 
-able to build a website for them. 
-If you don't mind I'll start working 
+This is the week 7 coding assignment. As a but of fyi, I am part of a 
+501c3 historical reenactment group and by the end of this I would like
+to be able to build a website for them. If you don't mind I'll start working 
 on it here. :D
 Thank you for your time.`)
 
 alert(`Greetings Program, 
-This is the week 7 coding assignment. 
-As a bit of fyi, I am part of a 501c3 
-historical reenactment group and by 
-the end of this I would like to be 
-able to build a website for them. 
-If you don't mind I'll start working 
+This is the week 7 coding assignment. As a but of fyi, I am part of a 
+501c3 historical reenactment group and by the end of this I would like
+to be able to build a website for them. If you don't mind I'll start working 
 on it here. :D
 Thank you for your time.`)
 
-//Magic Cannonball
+//Magic Cannonball Js game
 
 let userQuestion = ""
 console.log(userQuestion)
@@ -55,4 +49,4 @@ default:
 }
 return alert(cannonBall)
 }
-console.log(magicCannonball(cannonBall))
+//console.log(magicCannonball(cannonBall))
