f260ba235d95501426acf665e7219de1b492c85a
RKMellinger <rkrismellinger@gmail.com>
2022-09-18 20:14:45 -0700

    week 8 update

--- Week-08-CSS/Week  coding assignment/index.js
+++ Week-08-CSS/Week  coding assignment/index.js
@@ -5,16 +5,16 @@ to be able to build a website for them. If you don't mind I'll start working
 on it here. :D
 Thank you for your time.`)
 
-alert(`Greetings, 
-This is the week 7 coding assignment. As a but of fyi, I am part of a 
-501c3 historical reenactment group and by the end of this I would like
-to be able to build a website for them. If you don't mind I'll start working 
-on it here. :D
-Thank you for your time.`)
+// alert(`Greetings, 
+// This is the week 7 coding assignment. As a but of fyi, I am part of a 
+// 501c3 historical reenactment group and by the end of this I would like
+// to be able to build a website for them. If you don't mind I'll start working 
+// on it here. :D
+// Thank you for your time.`)
 
 //Magic Cannonball Js game
 
-let userQuestion = ""
+// let userQuestion = ""
 let randomNumber = Math.floor(Math.random() * 8);
 let cannonBall = ""
 let myButton = document.getElementById("submit");
@@ -50,9 +50,7 @@ default:
 }
 magicCannonball()
 console.log(myButton)
-
-myButton.addEventListener("click", function () {
-
+myButton.addEventListener("click", function(){
   alert(cannonBall)
 })
 console.log(userQuestion)
