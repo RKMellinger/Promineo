85b3f0ecc19a6ae20e1c339c27739c2c7d005edc
RKMellinger <rkrismellinger@gmail.com>
2022-10-04 19:25:02 -0700

    week 10 update 2.3

--- Week-10-JavaScript_and_DOM_Manipulation/Week  coding assignment/week.js
+++ Week-10-JavaScript_and_DOM_Manipulation/Week  coding assignment/week.js
@@ -1,37 +1,17 @@
-class Post{
-  constructor(customerName, customerEmail, customerComment){
-    this.name=customerName
-    this.email=customerEmail
-    this.comment=customerComment
-    this.array1 = []
-      }
-}
-let postId = 0;
-let customerPost = []
-
 let button = document.getElementById("new-customer-btn");
-// let customerName = document.getElementById("new-customer-name").value;
-// let customerEmail = document.getElementById("new-customer-email").value;
-// let customerComment = document.getElementById("new-customer-comment").value;
 
-
-function onClick(id, action) {
-  let element = document.getElementById(id);
-  element.addEventListener("click", action);
-  return element;
-}
-
-button.addEventListener('click', ()=>{
+button.addEventListener('click', (event)=>{
+  event.preventDefault()
   let customerName = document.getElementById("new-customer-name").value;
   let customerEmail = document.getElementById("new-customer-email").value;
   let customerComment = document.getElementById("new-customer-comment").value;
-  console.log(customerName.value);
-  console.log(customerEmail.value)
-  console.log(customerComment.value)
+  console.log(customerName);
+  console.log(customerEmail)
+  console.log(customerComment)
+  let t = document.getElementById("comment-table")
   let tr = document.createElement("tr")
   let td = document.createElement('td')
   td.innerText = customerName
-  let t = document.getElementById("comment-table")
   t.appendChild(tr)
   tr.appendChild(td)
   td = document.createElement('td')
@@ -44,23 +24,3 @@ customerName = document.getElementById("new-customer-name").value="";
 customerEmail = document.getElementById("new-customer-email").value="";
 customerComment = document.getElementById("new-customer-comment").value="";
 } )
-
-
-// document.getElementById("remove").addEventListener("click", () => {
-//   let idToRemove = document.getElementById("remove-id").value;
-//   let elementToRemove = document.getElementById(idToRemove);
-//   elementToRemove.parentNode.removeChild(elementToRemove);
-//   document.getElementById("remove-id").value = "";
-// });
-
-// 
-
-// document.getElementById("add").addEventListener("click", () => {
-//   var parent = document.getElementById("paragraphs");
-//   var newElement = document.createElement("p");
-//   newElement.innerHTML = document.getElementById("new-text").value;
-//   newElement.setAttribute("id", id++);
-//   parent.appendChild(newElement);
-//   document.getElementById("new-text").value = "";
-// });
-// console.log(id);
