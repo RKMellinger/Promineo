5a77525dc727c8c14b708486205a826563507f6a
RKMellinger <rkrismellinger@gmail.com>
2022-10-20 11:15:56 -0700

    week 12 update 4.0

--- Week-12-Project/Week  coding assignment/week.js
+++ Week-12-Project/Week  coding assignment/week.js
@@ -1,26 +1,65 @@
+// Initial creation for variables
+
 let button = $("#new-customer-btn");
+let editBtn = $("#editBtn")
+let removeBtn = $("#removeBtn")
+let customerCount = 2
+let reservations = []
 
-var homePortURL = $.get('https://63502b28df22c2af7b657cd0.mockapi.io/current/reservations')
 
+// mockapi link and variables for the site/functions
+let getPortURL = ('https://63502b28df22c2af7b657cd0.mockapi.io/current/reservations')
+
+
+
+// initial functions to collect and display information.
 button.on('click', (event) => {
   event.preventDefault()
+
   let customerName = $("#new-customer-name").val();
   let customerEmail = $("#new-customer-email").val();
   let customerQty = $("#new-customer-qty").val();
   let customerRequest = $("#new-customer-request").val();
 
-
+  //Console logs to confirm data.
+  console.log(customerCount);
   console.log(customerName);
   console.log(customerEmail)
   console.log(customerQty);
   console.log(customerRequest)
+  console.log(reservations);
 
-  let newEntry = `<tr><td>${customerName} </td><td>${customerEmail}</td><td>${customerQty}</td><td>${customerRequest}</td></tr>`;
+  reservations.push(customerCount, customerName, customerEmail, customerQty, customerRequest)
+
+  let newEntry =
+    `<tr><td>${customerName} </td><td>${customerEmail}</td><td>${customerQty}</td><td>${customerRequest}</td>
+  <td><button class="btn btn-primary" id="editBtn">Edit</button><button class="btn btn-danger" id="removeBtn">Delete</button></td></tr>`;
 
   $("#reservation-table").append(newEntry);
+  customerCount++
 
   customerName = $("#new-customer-name").val("");
   customerEmail = $("#new-customer-email").val("");
   customerQty = $("#new-customer-qty").val("");
   customerRequest = $("#new-customer-request").val("");
+})
+function getAllRes() {
+  return $.get(getPortURL);
+}
+function getMyRes(id) {
+  return $.get(this.url + `/${id}`);
+}
+function createRes(house) {
+  return $.post(this.url, house);
+}
+
+
+
+editBtn.on('click', (event) => {
+  event.preventDefault()
+})
+
+removeBtn.on('click', (event) => {
+  event.preventDefault()
+
 })
