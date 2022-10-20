107a7c7437d815336acd5d47bd33f4b7996e76c1
RKMellinger <rkrismellinger@gmail.com>
2022-10-20 15:28:26 -0700

    week 12 update 4.1

--- Week-12-Project/Week  coding assignment/week.js
+++ Week-12-Project/Week  coding assignment/week.js
@@ -3,8 +3,9 @@
 let button = $("#new-customer-btn");
 let editBtn = $("#editBtn")
 let removeBtn = $("#removeBtn")
-let customerCount = 2
 let reservations = []
+let customerCount
+
 
 
 // mockapi link and variables for the site/functions
@@ -12,6 +13,11 @@ let getPortURL = ('https://63502b28df22c2af7b657cd0.mockapi.io/current/reservati
 
 
 
+let tableServer = $(reservations).on("load", () => {
+  for (var i = 0; i < reservations.length; i++) {
+    newEntry += `<tr><td> ${reservations[i].customerCount}</td><td>${reservations[i].customerName}</td><td>${reservations[i].customerEmail}</td><td>${reservations[i].customerQty}</td><td>${reservations[i].customerRequest}</td><td><button class='btn btn-primary' id='editBtn'>Edit</button><button class='btn btn-danger' id='removeBtn'>Delete</button></td></tr>`;
+  }
+});
 // initial functions to collect and display information.
 button.on('click', (event) => {
   event.preventDefault()
@@ -24,42 +30,45 @@ button.on('click', (event) => {
   //Console logs to confirm data.
   console.log(customerCount);
   console.log(customerName);
-  console.log(customerEmail)
+  console.log(customerEmail);
   console.log(customerQty);
-  console.log(customerRequest)
+  console.log(customerRequest);
   console.log(reservations);
 
   reservations.push(customerCount, customerName, customerEmail, customerQty, customerRequest)
 
   let newEntry =
-    `<tr><td>${customerName} </td><td>${customerEmail}</td><td>${customerQty}</td><td>${customerRequest}</td>
-  <td><button class="btn btn-primary" id="editBtn">Edit</button><button class="btn btn-danger" id="removeBtn">Delete</button></td></tr>`;
-
+    `<tr><td>${customerName} </td><td>${customerEmail}</td><td>${customerQty}</td><td>${customerRequest}</td><td><button class="btn btn-primary" id="editBtn">Edit</button><button class="btn btn-danger" id="removeBtn">Delete</button></td></tr>`;
   $("#reservation-table").append(newEntry);
   customerCount++
-
   customerName = $("#new-customer-name").val("");
   customerEmail = $("#new-customer-email").val("");
   customerQty = $("#new-customer-qty").val("");
   customerRequest = $("#new-customer-request").val("");
+
+
 })
+
+
 function getAllRes() {
-  return $.get(getPortURL);
-}
-function getMyRes(id) {
-  return $.get(this.url + `/${id}`);
-}
-function createRes(house) {
-  return $.post(this.url, house);
+  return $.get(getPortURL).then((response) => { reservations.push(response) });
 }
 
+getAllRes()
+// function getMyRes(id) {
+//   return $.get(this.url + `/${id}`);
+// }
+// function createRes(house) {
+//   return $.post(this.url, house);
+// }
 
 
-editBtn.on('click', (event) => {
-  event.preventDefault()
-})
 
-removeBtn.on('click', (event) => {
-  event.preventDefault()
+// editBtn.on('click', (event) => {
+//   event.preventDefault()
+// })
 
-})
+// removeBtn.on('click', (event) => {
+//   event.preventDefault()
+
+// })
