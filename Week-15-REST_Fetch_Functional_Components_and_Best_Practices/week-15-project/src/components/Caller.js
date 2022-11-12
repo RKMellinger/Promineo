c499b3626411a7bcaed6fc14f8c3effbdeda963b
R.K. Mellinger <rkrismellinger@gmail.com>
2022-11-11 17:03:17 -0800

    Week 15 update 5.2

--- Week-15-REST_Fetch_Functional_Components_and_Best_Practices/week-15-project/src/components/Caller.js
+++ Week-15-REST_Fetch_Functional_Components_and_Best_Practices/week-15-project/src/components/Caller.js
@@ -6,6 +6,5 @@ export default function Caller() {
   fetch("https://63502b28df22c2af7b657cd0.mockapi.io/current/reservations")
     .then((res) => res.json())
     .then((data) => (data = returnedData));
-  return <div></div>;
+  return console.log(returnedData);
 }
-console.log(returnedData);
